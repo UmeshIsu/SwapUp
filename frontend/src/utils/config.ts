bf054cc0fa13ba93c030288fd36c6eeabb48d042
Vihara Senanayake <viharasenanayake402@gmail.com>
2026-03-20 16:30:28 +0530

    Moidying the ports accordingly

--- frontend/src/utils/config.ts
+++ frontend/src/utils/config.ts
@@ -7,16 +7,16 @@ const EMULATOR_IP = '10.0.2.2';
 export const getBaseUrl = (): string => {
     // If it's a physical device, use the LAN IP
     if (Constants.isDevice) {
-        return `http://${LAN_IP}:5001`;
+        return `http://${LAN_IP}:5000`;
     }
 
     // If it's an emulator/simulator, use the standard loopback
     // 10.0.2.2 is for Android emulator, localhost is usually fine for iOS simulator
     if (Platform.OS === 'android') {
-        return `http://${EMULATOR_IP}:5001`;
+        return `http://${EMULATOR_IP}:5000`;
     }
 
-    return `http://${LAN_IP}:5001`;
+    return `http://${LAN_IP}:5000`;
 };
 
 export const API_BASE_URL = `${getBaseUrl()}/api`;
