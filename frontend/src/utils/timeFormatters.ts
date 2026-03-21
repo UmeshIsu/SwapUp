5d627e50b7831bca5c527d744bd246e9c38d3a20
ashwin-miron <ashwinmiron14@gmail.com>
2026-03-22 00:24:27 +0530

    add afternoon check helper

--- frontend/src/utils/timeFormatters.ts
+++ frontend/src/utils/timeFormatters.ts
@@ -3,3 +3,4 @@ export const getHour = (d: Date) => d.getHours();
 export const getMinute = (d: Date) => d.getMinutes();
 export const getSecond = (d: Date) => d.getSeconds();
 export const isMorning = (d: Date) => d.getHours() < 12;
+export const isAfternoon = (d: Date) => d.getHours() >= 12;
