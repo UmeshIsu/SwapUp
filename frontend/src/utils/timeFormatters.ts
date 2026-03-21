85669e14e2573ae41cbec6df3b8435f1f27907aa
ashwin-miron <ashwinmiron14@gmail.com>
2026-03-22 00:24:27 +0530

    add second getter utility

--- frontend/src/utils/timeFormatters.ts
+++ frontend/src/utils/timeFormatters.ts
@@ -1,3 +1,4 @@
 export const formatTime = (d: Date) => d.toISOString();
 export const getHour = (d: Date) => d.getHours();
 export const getMinute = (d: Date) => d.getMinutes();
+export const getSecond = (d: Date) => d.getSeconds();
